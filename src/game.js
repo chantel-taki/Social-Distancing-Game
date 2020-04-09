57ece5e6cf73d4751199f8bc1a700d7e1c1df50d
Chantel Taki <chanteltaki@gmail.com>
2020-04-09 11:53:00 -0400

    fixed item collection

--- src/game.js
+++ src/game.js
@@ -1,103 +1,92 @@
-
 class Game {
-    constructor(){
-      // Creates arrays to push obstacles/people
-        this.obstacles = [];
-        this.people = [];
-        
-        this.startGame = false;
-        this.endGame = false;
-        this.score = 0;
+  constructor() {
+    // Creates arrays to push obstacles/people
+    this.obstacles = [];
+    this.people = [];
+    this.startGame = false;
+    this.endGame = false;
+    this.score = 0;
+  }
+  initialize() {
+    //initialize/generate the player and background
+    // if(this.startGame = true){
+    this.player = new Player();
+    this.background = new Background();
+    this.items = [];
+    //  }
+  }
+  // setup the player in game
+  setup() {
+    this.player.setup();
+  }
+  display() {
+    //clear the display after movements
+    //clear();
+    //display the background & player
+    this.background.display();
+    this.player.display();
+    textSize(100);
+    text("Score: " + this.score, 15, 80);
+    //push obstacles  to array at frame count
+    if (frameCount % 900 === 0) {
+      this.obstacles.push(new Obstacles());
     }
-
-    initialize(){
-      //initialize/generate the player and background
-        
-       // if(this.startGame = true){
-          this.player = new Player();
-          this.background = new Background();
-          this.items = [];
-      //  }
-        
+    this.obstacles.forEach((obstacle) => {
+      obstacle.display();
+    });
+    // check if collided with player, if so, end the game
+    this.obstacles = this.obstacles.filter((obstacle) => {
+      if (obstacle.collision(this.player)) {
+        this.endGame = true;
+      } else {
+        return !obstacle.collision(this.player);
+      }
+    });
+    //push people to array at frame count
+    if (frameCount % 1250 === 0) {
+      this.people.push(new People());
     }
-
-    // setup the player in game
-    setup(){
-        this.player.setup();
-         }
-
-    display(){
-       //clear the display after movements
-        //clear();
-        //display the background & player
-        this.background.display();
-        this.player.display();
-        textSize(100);
-        text("Score: " + this.score, 15, 80);
-
-        //push obstacles  to array at frame count
-        if (frameCount % 900 === 0) {
-          this.obstacles.push(new Obstacles());
-         }
-         this.obstacles.forEach((obstacle) => {
-           obstacle.display();
-         });
-
-         // check if collided with player, if so, end the game
-         this.obstacles = this.obstacles.filter((obstacle) => {
-           if (obstacle.collision(this.player)){
-             this.endGame = true;
-          } else{
-           return !obstacle.collision(this.player);
-          }
-         }); 
-
-         //push people to array at frame count
-         if (frameCount % 1250 === 0) {
-           this.people.push(new People());
-          }
-          this.people.forEach((person) => {
-            person.display();
-          });
-
-          // check if collided with player, if so, end the game
-          this.people = this.people.filter((person) => { 
-            if (person.collision(this.player)){
-             this.endGame = true;
-          } else {
-           return !person.collision(this.player);
-          }
-            
-          });
-
-                 //push items to array at frame count
-                 if (frameCount % 200 === 0) {
-                   this.items.push(new Items());
-                  }
-                
-                  //display items
-                    this.items.forEach((item) => {
-                    item.display();
-                  });
-
-                  //filter and hide items once collected
-                  this.items.filter((item, index) => { 
-                    if (item.collect(this.player)){
-                    // console.log("collected");
-                     item.img.hide();
-                     this.score += 1;
-                   }
-                   if (this.endGame){
-                    item.img.hide();
-                    return false
-                  }
-                  else{
-                    return true
-                  }
-                  })
-
-                  if(this.endGame){
+    this.people.forEach((person) => {
+      person.display();
+    });
+    // check if collided with player, if so, end the game
+    this.people = this.people.filter((person) => {
+      if (person.collision(this.player)) {
+        this.endGame = true;
+      } else {
+        return !person.collision(this.player);
+      }
+    });
+    //push items to array at frame count
+    if (frameCount % 200 === 0) {
+      this.items.push(new Items());
+    }
+    //display items
+    this.items.forEach((item) => {
+      item.display();
+    });
+    //filter and hide items once collected
+    console.log(this.items.length);
+    this.items = this.items.filter((item, index) => {
+      if (item.collect(this.player)) {
+        // console.log("collected");
+        console.log("hello");
+        item.img.hide();
+        this.score += 1;
+        return false;
+      } else {
+        return true;
+      }
+    });
+    console.log(this.items.length);
+    this.items.forEach((item) => {
+      if (this.endGame) {
+        item.img.hide();
+        document.querySelector("canvas").style.visibility = "hidden";
+      }
+    });
+    /*  if(this.endGame){
                     noLoop();
-                  }
-              }
-            }
+                  } */
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,72 +1,62 @@
-
 let game = new Game();
 //starting screen
 let startImg;
 let endImg;
-
-
-
-function preload (){
-  soundFormats('mp3', 'ogg');
- mySound = loadSound('assets/happy.mp3')
- coughSound = loadSound('assets/cough.mp3')
- jumpSound = loadSound('assets/Jump.mp3')
- game.initialize();
- startImg = createImg("assets/StartScreen.gif").hide();
-//// endImg = createImg("assets/EndScreen.gif").hide();
+function preload() {
+  soundFormats("mp3", "ogg");
+  mySound = loadSound("assets/happy.mp3");
+  coughSound = loadSound("assets/cough.mp3");
+  jumpSound = loadSound("assets/Jump.mp3");
+  game.initialize();
+  startImg = createImg("assets/StartScreen.gif").hide();
+  endImg = createImg("assets/EndScreen.gif").hide();
 }
-
-function setup() { 
+function setup() {
   createCanvas(1920, 1080);
   game.setup();
   mySound.play();
   mySound.loop();
   mySound.setVolume(0.3);
-  
-
-  } 
-  
-  function draw() { 
+}
+function draw() {
+  clear();
+  if (game.startGame && !game.endGame) {
+    game.display();
+    //console.log(frameRate());
+    frameRate(500);
+  } else if (!game.startGame && !game.endGame) {
+    startImg.show();
+  }
+  if (game.endGame) {
+    endImg.show();
+    game.player.img.hide();
+    startImg.hide();
+    coughSound.play();
+    coughSound.loop();
+    coughSound.setVolume(1);
+    game.items.forEach((item) => {
+      item.img.hide();
+    });
+    noLoop();
+  }
+}
+function keyPressed() {
+  // W key
+  if (keyCode === 87) {
+    game.player.jump();
+    jumpSound.play();
     clear();
-    if (game.startGame && !game.endGame){
-      game.display();
-      console.log(frameRate());
-      frameRate(120);
-    }  else if (!game.startGame && !game.endGame){
-      startImg.show();
-    }
-   if (game.endGame){
-      endImg.show();
-      game.player.img.hide();
-      startImg.hide();
-      coughSound.play();
-      coughSound.loop();
-      coughSound.setVolume(1);
-      noLoop();
-    }
- 
   }
-
-  function keyPressed(){
-    // W key
-    if (keyCode === 87) {
-      game.player.jump();
-      jumpSound.play();
-      clear();
-    }
-    // S key
-    if (keyCode === 83) {
-      game.player.duck();
-    }
-
-    if (keyCode === 13) {
-      game.startGame = true;
-      startImg.hide();
-      console.log("start");
-    }
-    if (keyCode === 13 && game.endGame === true) {
-      window.location.reload();
-    }
-
-
+  // S key
+  if (keyCode === 83) {
+    game.player.duck();
   }
+  if (keyCode === 13) {
+    game.startGame = true;
+    startImg.hide();
+    // console.log("start");
+  }
+  if (keyCode === 13 && game.endGame === true) {
+    window.location.reload();
+  }
+}
